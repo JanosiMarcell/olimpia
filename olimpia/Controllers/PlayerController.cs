091fbb85bfe20a804be270ec34864a62b62c97e1
JanosiMarcell <janosim@kkszki.hu>
2024-11-11 09:55:17 +0100

    post endpoints are ready to use

---
+++ olimpia/Controllers/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using olimpia.Models;
+
+namespace olimpia.Controllers
+{
+    [Route("player")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<Player> Post(CreatePlayerDto createPlayer)
+        {
+            var player = new Player
+            {
+                Id = Guid.NewGuid(),
+                Name = createPlayer.Name,
+                Age = createPlayer.Age,
+                Weight = createPlayer.Weight,
+                Height =createPlayer.Height,
+                CreatedTime = DateTime.Now,
+            };
+            if (player != null)
+            {
+                using (var context = new OlimpiaContext())
+                {
+                    context.Players.Add(player);
+                    context.SaveChanges();
+                    return StatusCode(201, player);
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
